e44fb1c1b64b1b9cf23bc350a40767b1db3e23a4
emmanueltan <emmanueltan2000@gmail.com>
2025-06-05 12:55:21 +0800

    add script to smoke test bwa

---
+++ tests/bwa_smoke_test.sh
@@ -0,0 +1,7 @@
+REFERENCE="./data/smoke-test/chr20.fa.gz"
+REFERENCE_INDEX="./data/smoke-test/chr20.fa.index"
+READS="./data/smoke-test/H06HDADXX130110.1.ATCACGAT.20k_reads_1.fastq"
+OUTPUT="./tests/out.sam"
+
+./bwa-mem2-bin/bwa-mem2 index -p $REFERENCE_INDEX $REFERENCE
+./bwa-mem2-bin/bwa-mem2 mem -t 4 $REFERENCE_INDEX $READS > $OUTPUT
