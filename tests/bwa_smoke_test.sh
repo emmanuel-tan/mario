a6e5a07c23a1b6f4861ee8574769671ca80ca0b2
emmanueltan <emmanueltan2000@gmail.com>
2025-06-05 13:25:27 +0800

    fix path to binary in smoke-test script, add step in workflow to print paths for debugging

--- tests/bwa_smoke_test.sh
+++ tests/bwa_smoke_test.sh
@@ -3,5 +3,5 @@ REFERENCE_INDEX="./data/smoke-test/chr20.fa.index"
 READS="./data/smoke-test/H06HDADXX130110.1.ATCACGAT.20k_reads_1.fastq"
 OUTPUT="./tests/out.sam"
 
-./bwa-mem2-bin/bwa-mem2/bwa-mem2 index -p $REFERENCE_INDEX $REFERENCE
-./bwa-mem2-bin/bwa-mem2/bwa-mem2 mem -t 4 $REFERENCE_INDEX $READS > $OUTPUT
+./bwa-mem2-bin/bwa-mem2 index -p $REFERENCE_INDEX $REFERENCE
+./bwa-mem2-bin/bwa-mem2 mem -t 4 $REFERENCE_INDEX $READS > $OUTPUT
